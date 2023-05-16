53f5cc3cd415bbe7e337d1ff49d464828cd2691f
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-05-15 22:13:58 -0300

    adding background-color to html

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-// import logo from './logo.svg';
-import "./App.css";
+import React from "react";
+import './App.css';
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
-import React from "react";
 
 function App() {
   const [darkMode, setDarkMode] = React.useState(true);
+
   function toggleDarkMode() {
     setDarkMode((prevMode) => !prevMode);
   }
+
   return (
-    <div className="App">
-      <Navbar 
-        darkMode={darkMode} 
-        toggleDarkMode={toggleDarkMode} />
-      <Main 
-        darkMode={darkMode} />
+    <div className={`App ${darkMode ? 'App-dark' : 'App-light'}`}>
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
